fc4a6cbef888ee8814dd080a67f6981a6de03f85
Derek Kinsman <derekkinsman@D.local>
2015-03-20 11:14:26 -0400

    Setup stuff.

--- www/discovery.js
+++ www/discovery.js
@@ -1,8 +1,7 @@
-var exec = require('cordova/exec');
 var Discovery = function() {};
 
 Discovery.prototype.identify = function(success, fail) {
-    exec(success, fail, "Discovery", "identify", []);
+    cordova.exec(success, fail, "Discovery", "identify", []);
 };
 
 var discovery = new Discovery();
