55141c41ab83159845259245cee5438a0d877419
Derek Kinsman <derekkinsman@D.local>
2015-03-20 10:54:11 -0400

    Setup stuff.

--- www/discovery.js
+++ www/discovery.js
@@ -1,18 +1,8 @@
-var argscheck = require('cordova/argscheck'),
-    channel = require('cordova/channel'),
-    utils = require('cordova/utils'),
-    exec = require('cordova/exec'),
-    cordova = require('cordova');
+var Discovery = function() {};
 
-/**
- * Identify a broadcast service.
- */
- 
-var Discovery = {
-  identify: function(successCallback, errorCallback, opts) {
-    console.log('Doing identify', opts);
-    exec(successCallback, errorCallback, "Discovery", "identify", [opts]);
-  }
-}
+Discovery.prototype.say = function(success, fail) {
+    cordova.exec(success, fail, "Discovery", "identify", []);
+};
 
-module.exports = Discovery;
+var discovery = new Discovery();
+module.exports = discovery;
