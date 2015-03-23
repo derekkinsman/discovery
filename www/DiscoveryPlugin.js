c0d6972879b96fdb3648ffd7e4c32b34b3a8c36b
Derek Kinsman <derekkinsman@D.local>
2015-03-23 16:50:14 -0400

    Corrections to DiscoveryPlugin.js.

--- www/DiscoveryPlugin.js
+++ www/DiscoveryPlugin.js
@@ -1,25 +1,11 @@
 var exec = require('cordova/exec');
 
-function Discovery() {}
+var Discovery = {
 
-Discovery.prototype.initChat = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Discovery", "initChat", []);
+  identify: function(serviceName, serviceType, successCallback, errorCallback) {
+      exec(successCallback, errorCallback, "Discovery", "identify", [serviceName, serviceType]);
+  }
+
 };
 
-Discovery.prototype.advertizeChat = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Discovery", "advertizeChat", []);
-};
-
-Discovery.prototype.identify = function(serviceName, serviceType, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Discovery", "identify", [serviceName, serviceType]);
-};
-
-Discovery.prototype.connectChat = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Discovery", "connectChat", []);
-};
-
-Discovery.prototype.sendChatMessage = function(messageString, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "Discovery", "sendChatMessage", [messageString]);
-};
-
-module.exports = new Discovery();
+module.exports = Discovery;
