a66cc187dd20d84821f480b0d6c894e33606788a
Derek Kinsman <derekkinsman@D.local>
2015-03-23 11:37:31 -0400

    Rewriting NSD. Fixing from a kernel panic.

--- www/DiscoveryPlugin.js
+++ www/DiscoveryPlugin.js
@@ -3,23 +3,23 @@ var exec = require('cordova/exec');
 function Discovery() {}
 
 Discovery.prototype.initChat = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Discovery", "initChat", []);
+    exec(successCallback, errorCallback, "Discovery", "initChat", []);
 };
 
 Discovery.prototype.advertizeChat = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Discovery", "advertizeChat", []);
+    exec(successCallback, errorCallback, "Discovery", "advertizeChat", []);
 };
 
 Discovery.prototype.discoverChat = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Discovery", "discoverChat", []);
+    exec(successCallback, errorCallback, "Discovery", "discoverChat", []);
 };
 
 Discovery.prototype.connectChat = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Discovery", "connectChat", []);
+    exec(successCallback, errorCallback, "Discovery", "connectChat", []);
 };
 
 Discovery.prototype.sendChatMessage = function(messageString, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Discovery", "sendChatMessage", [messageString]);
+    exec(successCallback, errorCallback, "Discovery", "sendChatMessage", [messageString]);
 };
 
 module.exports = new Discovery();
