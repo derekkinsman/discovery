e0277f2b522da99cc24a8b58a6dce218cea9c689
Derek Kinsman <derekkinsman@D.local>
2015-03-25 13:11:36 -0400

    Fix.

--- src/android/com/siberia/plugin/NsdHelper.java
+++ src/android/com/siberia/plugin/NsdHelper.java
@@ -77,9 +77,7 @@ public class NsdHelper
       // The name of the service tells the user what they'd be
       // connecting to. It could be "Bob's Chat App".
       Log.d(TAG, "Same machine: " + this.serviceName);
-      mNsdManager.resolveService(service, this);
     } else if (service.getServiceName().contains(this.serviceName)) {
-      Log.d(TAG, "Same machine: " + this.serviceName);
       mNsdManager.resolveService(service, this);
     }
   }
